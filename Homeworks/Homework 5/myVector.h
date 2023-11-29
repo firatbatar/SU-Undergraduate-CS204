5364e1bdde675cc988d1c46901b8090687c5b99b
Fırat Batar <firat.o.batar@gmail.com>
2023-11-29 19:36:34 +0300

    Add processData

--- Homeworks/Homework 5/myVector.h
+++ Homeworks/Homework 5/myVector.h
@@ -1,19 +1,25 @@
+#include <cmath>
+#include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
 using namespace std;
 
+template <typename TV, typename TK>
+struct KeyValPairStruct {
+    TV value;
+    TK key;
+
+    KeyValPairStruct(TV v, TK k) : value(v), key(k){};
+};
+
 template <typename T1, typename T2>
 class myVector {
    private:
-    struct KeyValuePair {
-        T1 value;
-        T2 key;
-
-        KeyValuePair(T1 v, T2 k) : value(v), key(k){};
-    };
+    typedef KeyValPairStruct<T1, T2> keyValPair;
 
     string name;
-    vector<KeyValuePair> nodes;
+    vector<keyValPair> nodes;
 
     friend class Iterator;
 
@@ -22,10 +28,10 @@ class myVector {
        public:
         Iterator() {}
 
-        Iterator(vector<KeyValuePair> *it_p) : it(it_p) {}
+        Iterator(vector<keyValPair> *it_p) : it(it_p) {}
 
         bool find(T2 oldKey, myVector<T1, T2> &v) {
-            typename vector<KeyValuePair>::iterator iter = (*it).begin();
+            typename vector<keyValPair>::iterator iter = (*it).begin();
             for (; iter != (*it).end(); iter++) {
                 if (iter->key == oldKey) {
                     itr = iter;
@@ -39,15 +45,15 @@ class myVector {
         void replaceKey(T2 newKey) { itr->key = newKey; }
 
         void printVector() {
-            typename vector<KeyValuePair>::iterator iter = (*it).begin();
+            typename vector<keyValPair>::iterator iter = (*it).begin();
             for (; iter != (*it).end(); iter++) {
                 cout << "Value: " << iter->value << ", Key: " << iter->key << endl;
             }
         }
 
        private:
-        vector<KeyValuePair> *it;
-        typename vector<KeyValuePair>::iterator itr;
+        vector<keyValPair> *it;
+        typename vector<keyValPair>::iterator itr;
     };
 
     // Default constructor
@@ -56,11 +62,14 @@ class myVector {
     // Copy constructor
     myVector(const myVector &v) : name(v.name), nodes(v.nodes) {}
 
+    // Getter for nodes
+    const vector<keyValPair> &getNodes() const { return nodes; }
+
     void setName(string nm) { name = nm; }
     string getName() { return name; }
 
     void push_back(T1 value, T2 key) {
-        KeyValuePair new_node(value, key);
+        keyValPair new_node(value, key);
         nodes.push_back(new_node);
     }
 
@@ -89,7 +98,7 @@ class myVector {
         return *this;
     }
 
-    vector<KeyValuePair> *begin() { return &nodes; }
+    vector<keyValPair> *begin() { return &nodes; }
 
     bool isEmpty() { return nodes.size() == 0; }
 
@@ -107,5 +116,123 @@ class myVector {
         return true;
     }
 
-    void process_data() {}
+    void process_data() {
+        // Chek if value type is string
+        if constexpr (is_same<T1, string>::value) {
+            // Combine all values into one string
+            string combined = "";
+            for (int i = 0; i < nodes.size(); i++) {
+                combined += " " + nodes[i].value;
+            }
+
+            // Find most frequent character
+            vector<int> char_counts(256, 0);
+            for (int i = 0; i < combined.length(); i++) {
+                char_counts[(char)combined[i]]++;
+            }
+            char mostFrequentChar = 0;
+            for (int i = 0; i < char_counts.size(); i++) {
+                if (char_counts[i] > char_counts[mostFrequentChar]) {
+                    mostFrequentChar = i;
+                }
+            }
+
+            // Find most frequent word
+            istringstream wordStream(combined);
+            string word;
+            myVector<int, string> wordCounts;
+
+            while (wordStream >> word) {
+                if (wordCounts[word] == nullptr) {
+                    wordCounts.push_back(1, word);
+                }
+                else {
+                    (*wordCounts[word])++;
+                }
+            }
+
+            const vector<KeyValPairStruct<int, string>> &wordCountsNodes = wordCounts.getNodes();
+            string mostFrequentWord = wordCountsNodes[0].key;
+            for (int i = 0; i < wordCountsNodes.size(); i++) {
+                if (wordCountsNodes[i].value > *wordCounts[mostFrequentWord]) {
+                    mostFrequentWord = wordCountsNodes[i].key;
+                }
+            }
+
+            // Print results
+            cout << "Most frequent character: '" << mostFrequentChar << "' (Frequency: " << char_counts[mostFrequentChar] << ")" << endl;
+            cout << "Most frequent word: \"" << mostFrequentWord << "\" (Frequency: " << *wordCounts[mostFrequentWord] << ")" << endl;
+        }
+
+        // Chek if value type is arithmetic
+        if constexpr (is_arithmetic<T1>::value) {
+            // Find  mean, median, standard deviation, max and mode
+            vector<T1> values;
+            T1 sum = 0;
+
+            // Add all values to vector as sorted
+            for (int i = 0; i < nodes.size(); i++) {
+                T1 value = nodes[i].value;
+                bool inserted = false;
+                for (int j = 0; j < values.size(); j++) {
+                    if (value < values[j]) {
+                        values.insert(values.begin() + j, value);
+                        inserted = true;
+                        break;
+                    }
+                }
+                if (!inserted) values.push_back(value);
+
+                sum += value;  // Calculate sum at the same time
+            }
+
+            // Mean
+            double mean = (double)sum / (double)values.size();
+
+            // Median
+            double median;
+            if (values.size() % 2 == 0) {
+                median = (values[values.size() / 2 - 1] + values[values.size() / 2]) / 2.0;
+            }
+            else {
+                median = values[(values.size() - 1) / 2];
+            }
+
+            // Standard deviation
+            double std_dev = 0;
+            for (int i = 0; i < values.size(); i++) {
+                std_dev += pow(values[i] - mean, 2);
+            }
+            std_dev = pow(std_dev / values.size(), 0.5);
+
+            // Max
+            T1 max = values[values.size() - 1];
+
+            // Mode
+            T1 mode = values[0];
+            int mode_count = 1;
+            T1 current = values[0];
+            int current_count = 1;
+            for (int i = 1; i < values.size(); i++) {
+                if (values[i] == current) {
+                    current_count++;
+                }
+                else {
+                    if (current_count > mode_count) {
+                        mode = values[i - 1];
+                        mode_count = current_count;
+                    }
+                    current = values[i];
+                    current_count = 1;
+                }
+            }
+
+            // Print results
+            cout << "Mean: " << mean << endl;
+            cout << "Median: " << median << endl;
+            cout << "Standard deviation: " << std_dev << endl;
+            cout << "Max: " << max << endl;
+            cout << "Mode: " << mode << endl;
+        }
+    }
 };
